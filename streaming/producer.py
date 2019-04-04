56b33c633fbf0cfa341c80b27eb6273dd3e08776
Ubuntu <ubuntu@ip-10-0-0-19.us-west-2.compute.internal>
2019-04-04 22:19:10 +0000

    able to connect to s3

--- streaming/producer.py
+++ streaming/producer.py
@@ -1,10 +1,30 @@
 from kafka import KafkaProducer
 from json import dumps
+import boto3
+import lazyreader
+import time
+
+def get_s3_and_produce():
+    while True:
+    	s3 = boto3.client('s3')
+   	obj = s3.get_object(Bucket="han-ping-insight-bucket", Key="nyc_taxi_raw_data/STREAMING_FILE/MTA-Bus-Time-2014-08-01.txt")
+    	for line in lazyreader.lazyread(obj['Body'], delimiter='\n'):
+	    print line
+	    message = line.strip()
+	    #message = message.split()
+	    #print message
+	    time.sleep(0.001)	
+
+def produce():
+
+    producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=lambda x: dumps(x).encode('utf-8'))
+
+    for e in range(10):
+        data = "This is a book"
+        producer.send('taxi-stream-input', value=data)
+        producer.flush()
 
 
-producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=lambda x: dumps(x).encode('utf-8'))
+if __name__ == '__main__':
+    get_s3_and_produce()
 
-for e in range(10):
-    data = "a"
-    producer.send('taxi-stream-input', value=data)
-    producer.flush()
