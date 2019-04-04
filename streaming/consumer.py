7698cc1c4ba5b5a945e31412adc8e75ab83c1aeb
Ubuntu <ubuntu@ip-10-0-0-19.us-west-2.compute.internal>
2019-04-04 23:57:04 +0000

    change java file

--- streaming/consumer.py
+++ streaming/consumer.py
@@ -1,4 +1,5 @@
 from kafka import KafkaConsumer
 consumer = KafkaConsumer("taxi-stream-output", auto_offset_reset ='earliest')
 for msg in consumer:
+    print "!!"
     print msg
--- streaming/producer.py
+++ streaming/producer.py
@@ -9,20 +9,21 @@ def get_s3_and_produce():
     	s3 = boto3.client('s3')
    	obj = s3.get_object(Bucket="han-ping-insight-bucket", Key="nyc_taxi_raw_data/STREAMING_FILE/MTA-Bus-Time-2014-08-01.txt")
     	for line in lazyreader.lazyread(obj['Body'], delimiter='\n'):
-	    print line
+	    #print line
 	    message = line.strip()
-	    #message = message.split()
+	    message = message.split()
 	    #print message
-	    time.sleep(0.001)	
+	    #need to filter out the col 
+	    produce(message)
+	    time.sleep(1)	
 
-def produce():
+def produce(msg):
 
     producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=lambda x: dumps(x).encode('utf-8'))
 
-    for e in range(10):
-        data = "This is a book"
-        producer.send('taxi-stream-input', value=data)
-        producer.flush()
+       # data = "This is a book"
+    producer.send('taxi-stream-input', value=msg)
+    producer.flush()
 
 
 if __name__ == '__main__':
