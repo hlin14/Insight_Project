8f4dbdb294fe77f2422f1200c1f4372d3be34461
hlin14 <hlin14@ncsu.edu>
2019-04-04 21:30:43 -0700

    modify part of consumer and producer

--- streaming/consumer.py
+++ streaming/consumer.py
@@ -1,5 +1,27 @@
 from kafka import KafkaConsumer
-consumer = KafkaConsumer("taxi-stream-output", auto_offset_reset ='earliest')
-for msg in consumer:
-    print "!!"
-    print msg
+import psycopg2
+
+def start_consumer():
+	consumer = KafkaConsumer("taxi-stream-output", auto_offset_reset ='earliest')
+
+	for msg in consumer:
+
+		print msg
+
+		#connect to db, and save to db
+		postgres_password = os.environ["POSTGRESPASSWORD"]
+		conn_string = "host='10.0.0.10' dbname='ping_db' user='ping' password=" + postgres_password
+
+		conn = psycopg2.connect(conn_string)
+
+		cursor = conn.cursor()
+
+		#cursor.execute("INSERT INTO bus_testing VALUES (1, 5, 30, -74.1715, 40.6894, -74.1716, 40.6895);")
+
+		#if using select * from ...
+		#records = cursor.fetchall()
+
+
+
+if __name__ == '__main__':
+	start_consumer()
--- streaming/producer.py
+++ streaming/producer.py
@@ -5,27 +5,26 @@ import lazyreader
 import time
 
 def get_s3_and_produce():
-    while True:
-    	s3 = boto3.client('s3')
-   	obj = s3.get_object(Bucket="han-ping-insight-bucket", Key="nyc_taxi_raw_data/STREAMING_FILE/MTA-Bus-Time-2014-08-01.txt")
-    	for line in lazyreader.lazyread(obj['Body'], delimiter='\n'):
-	    #print line
-	    message = line.strip()
-	    message = message.split()
-	    #print message
-	    #need to filter out the col 
-	    produce(message)
-	    time.sleep(1)	
+	while True:
+		s3 = boto3.client('s3')
+		obj = s3.get_object(Bucket="han-ping-insight-bucket", Key="nyc_taxi_raw_data/STREAMING_FILE/MTA-Bus-Time-2014-08-01.txt")
+		for line in lazyreader.lazyread(obj['Body'], delimiter='\n'):
+			#print line
+			message = line.strip()
+			message = message.split()
+			#print message
+			#need to filter out the col 
+			produce(message)
+			time.sleep(1)
 
 def produce(msg):
 
-    producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=lambda x: dumps(x).encode('utf-8'))
+	producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=lambda x: dumps(x).encode('utf-8'))
 
-       # data = "This is a book"
-    producer.send('taxi-stream-input', value=msg)
-    producer.flush()
+	producer.send('taxi-stream-input', value=msg)
+	producer.flush()
 
 
 if __name__ == '__main__':
-    get_s3_and_produce()
+	get_s3_and_produce()
 
