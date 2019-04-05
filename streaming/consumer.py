71f0fb6f210708c631f2a3d670a22de35b378427
Ubuntu <ubuntu@ip-10-0-0-19.us-west-2.compute.internal>
2019-04-05 05:38:16 +0000

    can save into postgresql

--- streaming/consumer.py
+++ streaming/consumer.py
@@ -1,9 +1,12 @@
 from kafka import KafkaConsumer
 import psycopg2
-
+import os
+from datetime import datetime
+import random
 def start_consumer():
-	consumer = KafkaConsumer("taxi-stream-output", auto_offset_reset ='earliest')
-
+	#consumer = KafkaConsumer("taxi-stream-output", auto_offset_reset ='earliest')
+	consumer = KafkaConsumer("taxi-stream-output")
+	_id = 1
 	for msg in consumer:
 
 		print msg
@@ -14,10 +17,14 @@ def start_consumer():
 
 		conn = psycopg2.connect(conn_string)
 
-		cursor = conn.cursor()
-
-		#cursor.execute("INSERT INTO bus_testing VALUES (1, 5, 30, -74.1715, 40.6894, -74.1716, 40.6895);")
-
+		cur = conn.cursor()
+		dt = datetime(2015, 1, 1, 12, 30, 59, 0)
+		#need try catch, id choice
+		_id = random.randint(1,10001)
+		cur.execute("INSERT INTO bus_testing VALUES (%s, 12 , 12, -74.1715, 40.6894, %s, 40.6895);",[_id, dt])
+		conn.commit()
+		cur.close()
+		conn.close()
 		#if using select * from ...
 		#records = cursor.fetchall()
 
