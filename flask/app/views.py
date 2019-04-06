4f3c969adbea533b9874c590c021581f4f92b12f
Ubuntu <ubuntu@ip-10-0-0-4.us-west-2.compute.internal>
2019-04-06 21:04:36 +0000

    change flask view to able to get info in the db

--- flask/app/views.py
+++ flask/app/views.py
@@ -7,34 +7,31 @@ from flask_googlemaps import Map
 
 import os
 def connect_to_db():
-    postgres_password = os.environ["POSTGRESPASSWORD"]
-    conn_string = "host='10.0.0.10' dbname='ping_db' user='ping' password=" + postgres_password
+	postgres_password = os.environ["POSTGRESPASSWORD"]
+	conn_string = "host='10.0.0.10' dbname='ping_db' user='ping' password=" + postgres_password
 
-    conn = psycopg2.connect(conn_string)
+	conn = psycopg2.connect(conn_string)
 
-    cursor = conn.cursor()
+	cursor = conn.cursor()
 
-    cursor.execute("SELECT * FROM result_to_flask")
-    current = ""
-    recommend = ""
-    records = cursor.fetchall()
-    
-    return records
-    #return str(records[0])
+	cursor.execute("SELECT * FROM bus_testing WHERE bus_id = %s ORDER BY date_time DESC LIMIT 1;",[5945])
+	records = cursor.fetchall()
+	print records
+	return records
+	#return str(records[0])
 
 @app.route('/')
 @app.route('/index')
 def index():
-    #print records
-    records = connect_to_db()
-    client1 = {
-		"id":records[0][0],
-		"cur_long":float(records[0][1]),
-		"cur_lat":float(records[0][2]),
-		"rec_long":float(records[0][3]),
-		"rec_lat":float(records[0][4])
+	records = connect_to_db()
+
+	client1 = {
+		"id":1,
+		"cur_long":float(records[0][3]),
+		"cur_lat":float(records[0][4]),
+		"rec_long":-73.98418,
+		"rec_lat":40.748222
 		}
-    #return conn_string	
-    #return "Hello, World!"
-    GOOGLEMAPAPI = os.environ["GOOGLEMAPAPI"]
-    return render_template('example2.html', googleapi = GOOGLEMAPAPI, client1 = client1)
+
+	GOOGLEMAPAPI = os.environ["GOOGLEMAPAPI"]
+	return render_template('example2.html', googleapi = GOOGLEMAPAPI, client1 = client1)
