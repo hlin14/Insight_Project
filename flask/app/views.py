2349cb75ad98f5aec58e8257c542b64a6e588a08
Ubuntu <ubuntu@ip-10-0-0-4.us-west-2.compute.internal>
2019-04-06 22:30:49 +0000

    socket working to show on console

--- flask/app/views.py
+++ flask/app/views.py
@@ -1,4 +1,3 @@
-from app import app
 import psycopg2
 
 from flask import Flask, render_template
@@ -6,6 +5,13 @@ from flask_googlemaps import GoogleMaps
 from flask_googlemaps import Map
 
 import os
+
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+
 def connect_to_db():
 	postgres_password = os.environ["POSTGRESPASSWORD"]
 	conn_string = "host='10.0.0.10' dbname='ping_db' user='ping' password=" + postgres_password
@@ -20,8 +26,8 @@ def connect_to_db():
 	return records
 	#return str(records[0])
 
-@app.route('/')
-@app.route('/index')
+@app.route('/',methods=['GET', 'POST'])
+@app.route('/index',methods=['GET', 'POST'])
 def index():
 	records = connect_to_db()
 
@@ -35,3 +41,16 @@ def index():
 
 	GOOGLEMAPAPI = os.environ["GOOGLEMAPAPI"]
 	return render_template('example2.html', googleapi = GOOGLEMAPAPI, client1 = client1)
+
+def messageReceived(methods=['GET', 'POST']):
+    print('message was received!')
+
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('received my event: ' + str(json))
+    socketio.emit('my response', json, callback=messageReceived)
+
+
+if __name__ == '__main__':
+
+	socketio.run(app, host='0.0.0.0', debug = True)
