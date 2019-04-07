5385f564715d5cdd25df6338e08ed6ba7e51441c
Ubuntu <ubuntu@ip-10-0-0-4.us-west-2.compute.internal>
2019-04-07 00:39:24 +0000

    can get from user side and fetch from db but can not show on the map yet

--- flask/app/views.py
+++ flask/app/views.py
@@ -19,36 +19,48 @@ def connect_to_db():
 	conn = psycopg2.connect(conn_string)
 
 	cursor = conn.cursor()
-
-	cursor.execute("SELECT * FROM bus_testing WHERE bus_id = %s ORDER BY date_time DESC LIMIT 1;",[5945])
-	records = cursor.fetchall()
-	print records
-	return records
+	global cursor
+	#cursor.execute("SELECT * FROM bus_testing WHERE bus_id = %s ORDER BY date_time DESC LIMIT 1;",[5945])
+	#records = cursor.fetchall()
+	#print records
+	#return records
 	#return str(records[0])
 
 @app.route('/',methods=['GET', 'POST'])
 @app.route('/index',methods=['GET', 'POST'])
 def index():
-	records = connect_to_db()
+	connect_to_db()
 
-	client1 = {
-		"id":1,
-		"cur_long":float(records[0][3]),
-		"cur_lat":float(records[0][4]),
-		"rec_long":-73.98418,
-		"rec_lat":40.748222
-		}
+#	client1 = {
+#		"id":1,
+#		"cur_long":float(records[0][3]),
+#		"cur_lat":float(records[0][4]),
+#		"rec_long":-73.98418,
+#		"rec_lat":40.748222
+#		}
 
 	GOOGLEMAPAPI = os.environ["GOOGLEMAPAPI"]
-	return render_template('example2.html', googleapi = GOOGLEMAPAPI, client1 = client1)
+	return render_template('example2.html', googleapi = GOOGLEMAPAPI)
 
 def messageReceived(methods=['GET', 'POST']):
-    print('message was received!')
+	print 'message was received!'
 
 @socketio.on('my event')
 def handle_my_custom_event(json, methods=['GET', 'POST']):
-    print('received my event: ' + str(json))
-    socketio.emit('my response', json, callback=messageReceived)
+	print 'received my event: ' + str(json)
+	print json['message']
+	
+	global cursor
+	cursor.execute("SELECT * FROM bus_testing WHERE bus_id = %s ORDER BY date_time DESC LIMIT 1;",[float(json['message'])])
+	records = cursor.fetchall()
+
+	json = {
+		"long": float(records[0][3]),
+		"lat": float(records[0][4])
+	}	
+
+
+	socketio.emit('my response', json, callback=messageReceived)
 
 
 if __name__ == '__main__':
