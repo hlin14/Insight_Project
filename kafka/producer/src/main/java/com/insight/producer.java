57d715fa934185525e41971497b9299d9414b7d9
hlin14 <hlin14@ncsu.edu>
2019-04-16 15:06:42 -0700

    add read file in producer.java

--- kafka/producer/src/main/java/com/insight/producer.java
+++ kafka/producer/src/main/java/com/insight/producer.java
@@ -18,6 +18,11 @@ import java.time.format.DateTimeFormatter;
 
 import java.util.HashMap;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class producer {
     public static void main(String[] args) {
         Properties properties = new Properties();
@@ -33,76 +38,113 @@ public class producer {
 
         Producer<String, String> producer = new KafkaProducer<>(properties);
 
-        int i = 0;
         while (true) {
+            int i = 0;
             System.out.println("Producing batch: " + i);
             //init HaspMap
             HashMap<Integer, LocalDateTime> durTMap= new HashMap<Integer, LocalDateTime>();
             HashMap<Integer, Double> durDMap= new HashMap<Integer, Double>();
 
-
-
+            String csvFile = "MTA-Bus-Time-2014-08-01_small_csv.csv";
+            BufferedReader br = null;
+            String line = "";
+            String cvsSplitBy = ",";
 
             try {
-                Double latitude = 40.668602;
-                Double longtitude = -73.986697;
-                String timeReceived = "2014/8/1 4:00";
-                Integer busID = 469;
-                Double distanceAlong = 4135.347107;
-                Integer directionID = 1;
-                String phase = "IN_PROGRESS";
-                String routeID = "MTA NYCT_B63";
-                Double nextStopDistance = 2.631838042;
-                String nextStopID = "MTA_305423";
+                br = new BufferedReader(new FileReader(csvFile));
+                while ((line = br.readLine()) != null) {
 
-                //tranform time to localdatetime
-                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/M/d H:mm");
-                LocalDateTime formatDateTime = LocalDateTime.parse(timeReceived, formatter);
+                    String[] transaction = line.split(cvsSplitBy);
 
-                //init duration time(sec) and distance(meter)
-                Long durT = 0L;
-                Double durD = 0.0;
+                    if (i > 0) {
+                        try {
+                            //get data from the columns
+                            Double latitude = Double.valueOf(transaction[0]);
+                            Double longtitude = Double.valueOf(transaction[1]);
+                            String timeReceived = transaction[2];
+                            Integer busID = Integer.parseInt(transaction[3]);
+                            Double distanceAlong = Double.valueOf(transaction[4]);
+                            Integer directionID = Integer.parseInt(transaction[5]);
+                            String phase = transaction[6];
+                            String routeID = transaction[7];
+                            Double nextStopDistance = Double.valueOf(transaction[9]);
+                            String nextStopID = transaction[10];
 
-                if (phase.equals("IN_PROGRESS")){
-                    //update duration time
-                    if (!durTMap.containsKey(busID)) {
-                        durTMap.put(busID, formatDateTime);
-                    }
-                    else {
-                        LocalDateTime lastFormatDateTime=durTMap.get(busID);
-                        Duration duration = Duration.between(lastFormatDateTime, formatDateTime);
-                        Long durSec = duration.getSeconds();
-                        //make sure the time is correct
-                        if (durSec > 0) {
-                            durT = durSec;
+                            //tranform time to localdatetime
+                            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/M/d H:mm");
+                            LocalDateTime formatDateTime = LocalDateTime.parse(timeReceived, formatter);
+
+                            //init duration time(sec) and distance(meter)
+                            Long durT = 0L;
+                            Double durD = 0.0;
+
+                            //make sure the bus's phase is in "IN_PROGRESS"
+                            if (phase.equals("IN_PROGRESS")){
+                                //update duration time
+                                if (!durTMap.containsKey(busID)) {
+                                    durTMap.put(busID, formatDateTime);
+                                }
+                                else {
+                                    LocalDateTime lastFormatDateTime=durTMap.get(busID);
+                                    Duration duration = Duration.between(lastFormatDateTime, formatDateTime);
+                                    Long durSec = duration.getSeconds();
+                                    Long temp = 0L;
+                                    int compare =  durSec.compareTo(temp);
+                                    //make sure the time is correct
+                                    if (compare > 0) {
+                                        durT = durSec;
+                                    }
+                                    //if not correct, update the map
+                                    else {
+                                        durTMap.put(busID, formatDateTime);
+                                    }
+                                }
+
+                                //update duration distance
+                                if (!durDMap.containsKey(busID)) {
+                                    durDMap.put(busID, distanceAlong);
+                                }
+                                else {
+                                    Double lastDistanceAlong = durDMap.get(busID);
+                                    Double durDis = distanceAlong - lastDistanceAlong;
+                                    //make sure duration distance is correct
+                                    if (durDis > 0.0) {
+                                        durD = durDis;
+                                    }
+                                    //if not correct, update the map
+                                    else {
+                                        durDMap.put(busID, distanceAlong);
+                                    }
+                                }
+
+                            }
+
+                            producer.send(newTransaction(latitude, longtitude, formatDateTime, busID, distanceAlong, directionID, phase, routeID, nextStopDistance, nextStopID, durT, durD));
+                            Thread.sleep(100);
+                            producer.close();
+                            //producer.send(newTransaction(40.713702, -73.97967, 3928, 3974.805808, 0, "LAYOVER_DURING", "MTA NYCT_M22", 129.3308986, "MTA_903025"));
+                            //Thread.sleep(100);
+                            //i += 1;
+                        } catch (InterruptedException e) {
+                            break;
                         }
                     }
-
-                    //update duration distance
-                    if (!durDMap.containsKey(busID)) {
-                        durDMap.put(busID, distanceAlong);
-                    }
-                    else {
-                        Double lastDistanceAlong = durDMap.get(busID);
-                        Double durDis = distanceAlong - lastDistanceAlong;
-                        //make sure duration distance is correct
-                        if (durDis > 0) {
-                            durD = durDis;
-                        }
-                    }
-
+                    i += 1;
                 }
-
-                producer.send(newTransaction(latitude, longtitude, formatDateTime, busID, distanceAlong, directionID, phase, routeID, nextStopDistance, nextStopID, durT, durD));
-                Thread.sleep(100);
-                //producer.send(newTransaction(40.713702, -73.97967, 3928, 3974.805808, 0, "LAYOVER_DURING", "MTA NYCT_M22", 129.3308986, "MTA_903025"));
-                //Thread.sleep(100);
-                i += 1;
-            } catch (InterruptedException e) {
-                break;
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (br != null) {
+                    try {
+                        br.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
         }
-        producer.close();
     }
 
     public static ProducerRecord<String, String> newTransaction(Double latitude, Double longtitude, LocalDateTime formatDateTime, Integer busID, Double distanceAlong, Integer directionID, String phase, String routeID, Double nextStopDistance, String nextStopID, Long durT, Double durD) {
