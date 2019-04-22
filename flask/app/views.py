c0f726e9f32ca3ec2125a0b082ce387a0dd4c16d
Ubuntu <ubuntu@ip-10-0-0-19.us-west-2.compute.internal>
2019-04-22 21:49:15 +0000

    clean code

--- flask/app/views.py
+++ flask/app/views.py
@@ -1,4 +1,3 @@
-import psycopg2
 
 from flask import Flask, render_template
 from flask_googlemaps import GoogleMaps
@@ -35,97 +34,23 @@ def catch_event(json, methods=['GET', 'POST']):
 			json_sent.append({"busID":row[0], "long":float(row[2]), "lat":float(row[1])})
 			#print row[0], row[1], row[2]
 		
-		#json_sent = {"long": float(ret_long),"lat": float(ret_lat)}
-		#json_sent = [{"busID":123, "long":123, "lat":456},{"busID":456, "long":123, "lat":456}]
 		print json_sent
 		socketio.emit('my response', json_sent)
-		socketio.sleep(10)
+		socketio.sleep(5)
 
 
-def connect_to_db():
-	postgres_password = os.environ["POSTGRESPASSWORD"]
-	conn_string = "host='10.0.0.10' dbname='ping_db' user='ping' password=" + postgres_password
-
-	conn = psycopg2.connect(conn_string)
-
-	cursor = conn.cursor()
-	global cursor
-	#cursor.execute("SELECT * FROM bus_testing WHERE bus_id = %s ORDER BY date_time DESC LIMIT 1;",[5945])
-	#records = cursor.fetchall()
-	#print records
-	#return records
-	#return str(records[0])
 
 @app.route('/',methods=['GET', 'POST'])
 @app.route('/index',methods=['GET', 'POST'])
 def index():
-#	connect_to_db()
 	connect_to_cassandra()
-#	client1 = {
-#		"id":1,
-#		"cur_long":float(records[0][3]),
-#		"cur_lat":float(records[0][4]),
-#		"rec_long":-73.98418,
-#		"rec_lat":40.748222
-#		}
 
 	GOOGLEMAPAPI = os.environ["GOOGLEMAPAPI"]
 	return render_template('example2.html', googleapi = GOOGLEMAPAPI)
 
 def messageReceived(methods=['GET', 'POST']):
 	print 'message was received!'
-'''
-@socketio.on('my event')
-def handle_my_custom_event(json, methods=['GET', 'POST']):
-	print 'received my event: ' + str(json)
-	#print json['message']
-	
-	global cursor
-	cursor.execute("SELECT * FROM bus_testing WHERE bus_id = %s ORDER BY date_time DESC LIMIT 1;",[float(json['message'])])
-	records = cursor.fetchall()
 
-	json = {
-		"long": float(records[0][3]),
-		"lat": float(records[0][4])
-	}	
-
-
-	socketio.emit('my response', json, callback=messageReceived)
-'''
-
-'''
-q = []
-@socketio.on('my event')
-def set_interval(json, methods=['GET', 'POST']):
-        if  "" != json.get('message', ""):
-		q.append(json)
-                t = threading.Thread(target=func)
-                t.start()
-        else:
-
-                func()
-
-'''
-
-#@socketio.on('my event')
-def func(json, methods=['GET', 'POST']):
-	while True:
-#		json = q[-1]
-		print 'received my event: ' + str(json)
-		#print json['message']
-
-		global cursor
-		cursor.execute("SELECT * FROM bus_testing WHERE bus_id = %s ORDER BY date_time DESC LIMIT 1;",[float(json['message'])])
-		records = cursor.fetchall()
-
-		json_sent = {
-			"long": float(records[0][3]),
-			"lat": float(records[0][4])
-		}
-
-		print json_sent
-		socketio.emit('my response', json_sent)
-		socketio.sleep(0.8)
 if __name__ == '__main__':
 
 	socketio.run(app, host='0.0.0.0', debug = True)
--- kafka/core/src/main/java/com/insight/core.java
+++ kafka/core/src/main/java/com/insight/core.java
@@ -28,10 +28,8 @@ public class core {
         config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
 //        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
-        // we disable the cache to demonstrate all the "steps" involved in the transformation - not recommended in prod
         config.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, "0");
 
-        // Exactly once processing!!
         config.put(StreamsConfig.PROCESSING_GUARANTEE_CONFIG, StreamsConfig.EXACTLY_ONCE);
 
         // json Serde
@@ -68,23 +66,6 @@ public class core {
 
 
 
-        // create the initial json object for balances
-//        ObjectNode initialBalance = JsonNodeFactory.instance.objectNode();
-//        initialBalance.put("count", 0);
-//        initialBalance.put("balance", 0);
-//       initialBalance.put("time", Instant.ofEpochMilli(0L).toString());
-/*
-        KTable<String, JsonNode> bankBalance = bankTransactions
-                .groupByKey(Serdes.String(), jsonSerde)
-                .aggregate(
-                        () -> initialBalance,
-                        (key, transaction, balance) -> newBalance(transaction, balance),
-                        jsonSerde,
-                        "bank-balance-agg"
-                );
-
-        bankBalance.to(Serdes.String(), jsonSerde,"bank-balance-exactly-once");
-*/
         KafkaStreams streams = new KafkaStreams(builder, config);
         streams.cleanUp();
         streams.start();
@@ -96,16 +77,4 @@ public class core {
         Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
     }
 
-    private static JsonNode newBalance(JsonNode transaction, JsonNode balance) {
-        // create a new balance json object
-        ObjectNode newBalance = JsonNodeFactory.instance.objectNode();
-        newBalance.put("count", balance.get("count").asInt() + 1);
-        newBalance.put("balance", balance.get("balance").asInt() + transaction.get("amount").asInt());
-
-        Long balanceEpoch = Instant.parse(balance.get("time").asText()).toEpochMilli();
-        Long transactionEpoch = Instant.parse(transaction.get("time").asText()).toEpochMilli();
-        Instant newBalanceInstant = Instant.ofEpochMilli(Math.max(balanceEpoch, transactionEpoch));
-        newBalance.put("time", newBalanceInstant.toString());
-        return newBalance;
-    }
 }
